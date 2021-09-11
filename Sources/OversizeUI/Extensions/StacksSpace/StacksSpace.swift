7b6b23af14cb82e58d488aef8b94fe0c5e179366
Alexander Romanov <aromanov07@gmail.com>
2021-09-12 00:06:33 +0300

    Add pin wallet

--- Sources/OversizeUI/Extensions/StacksSpace/StacksSpace.swift
+++ Sources/OversizeUI/Extensions/StacksSpace/StacksSpace.swift
@@ -28,8 +28,16 @@ public extension LazyVStack {
     }
 }
 
+public extension LazyVGrid {
+     init(columns: [GridItem], alignment: HorizontalAlignment = .center, spacing: Space, pinnedViews: PinnedScrollableViews = .init(), @ViewBuilder content: () -> Content) {
+        self = .init(columns: columns, alignment: alignment, spacing: spacing.rawValue, pinnedViews: pinnedViews, content: content)
+    }
+}
+
 public extension Spacer {
     init(minLength: Space) {
         self = .init(minLength: minLength.rawValue)
     }
 }
+
+
