af196ff0c64944196390f285301d69e6e14a360a
Alexander Romanov <aromanov07@gmail.com>
2021-09-25 17:47:57 +0300

    Upd

--- Sources/OversizeUI/Controls/Toogle/Toogle.swift
+++ Sources/OversizeUI/Controls/Toogle/Toogle.swift
@@ -4,18 +4,3 @@
 //
 //  Created by alexander on 25.09.2021.
 //
-
-import Foundation
-
-struct CheckboxToggleStyle: ToggleStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        return HStack {
-            configuration.label
-            Spacer()
-            Image(systemName: configuration.isOn ? "checkmark.square" : "square")
-                .resizable()
-                .frame(width: 22, height: 22)
-                .onTapGesture { configuration.isOn.toggle() }
-        }
-    }
-}
