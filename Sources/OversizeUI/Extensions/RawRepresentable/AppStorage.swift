af5f21d8ed3faf153b10a5a5d5c822bc719258d1
Alexander Romanov <aromanov07@gmail.com>
2021-12-10 23:21:42 +0300

    Add extensions for @AppStorage

---
+++ Sources/OversizeUI/Extensions/RawRepresentable/AppStorage.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2021 Alexander Romanov
+// Created on 09.12.2021
+//
+
+import SwiftUI
+
+extension Date: RawRepresentable {
+    private static let formatter = ISO8601DateFormatter()
+
+    public var rawValue: String {
+        Date.formatter.string(from: self)
+    }
+
+    public init?(rawValue: String) {
+        self = Date.formatter.date(from: rawValue) ?? Date()
+    }
+}
+
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
