506efa02293545e0fdf10a8101b70b4b31292574
Alexander Romanov <aromanov07@gmail.com>
2023-02-28 01:04:44 +0300

    Add focused state for TextField styles

--- Sources/OversizeUI/Controls/TextEditor/TextEditorPlaceholderViewModifier.swift
+++ Sources/OversizeUI/Controls/TextEditor/TextEditorPlaceholderViewModifier.swift
@@ -6,11 +6,14 @@
 import SwiftUI
 
 public struct TextEditorPlaceholderViewModifier: ViewModifier {
-    private let placeholder: String
+    @Environment(\.theme) private var theme: ThemeSettings
     @Binding private var text: String
+    private let placeholder: String
+    private let isFocused: Bool
 
-    public init(placeholder: String, text: Binding<String>) {
+    public init(placeholder: String, text: Binding<String>, focused: Bool = false) {
         self.placeholder = placeholder
+        isFocused = focused
         _text = text
     }
 
@@ -21,24 +24,43 @@ public struct TextEditorPlaceholderViewModifier: ViewModifier {
             .headline()
             .onSurfaceHighEmphasisForegroundColor()
             .background {
-                RoundedRectangle(cornerRadius: .medium, style: .continuous)
-                    .fillSurfaceSecondary()
-                    .overlay(alignment: .topLeading) {
-                        if text.isEmpty {
-                            Text(placeholder)
-                                .headline()
-                                .onSurfaceDisabledForegroundColor()
-                                .opacity(0.7)
-                                .padding(.small)
-                        }
+                ZStack {
+                    RoundedRectangle(cornerRadius: .medium, style: .continuous)
+                        .fill(isFocused ? Color.surfacePrimary : Color.surfaceSecondary)
+                    overlay
+                }
+                .overlay(alignment: .topLeading) {
+                    if text.isEmpty {
+                        Text(placeholder)
+                            .headline()
+                            .onSurfaceDisabledForegroundColor()
+                            .opacity(0.7)
+                            .padding(.small)
                     }
+                }
             }
             .frame(minHeight: Space.xxxLarge.rawValue)
+    }
+
+    @ViewBuilder
+    var overlay: some View {
+        RoundedRectangle(cornerRadius: Radius.medium, style: .continuous)
+            .stroke(overlayBorderColor, lineWidth: isFocused ? 2 : CGFloat(theme.borderSize))
+    }
+
+    var overlayBorderColor: Color {
+        if isFocused {
+            return Color.accentColor
+        } else if theme.borderTextFields {
+            return Color.border
+        } else {
+            return Color.clear
+        }
     }
 }
 
 public extension View {
-    func textEditorPlaceholder(_ placeholder: String, text: Binding<String>) -> some View {
-        modifier(TextEditorPlaceholderViewModifier(placeholder: placeholder, text: text))
+    func textEditorPlaceholder(_ placeholder: String, text: Binding<String>, focused: Bool = false) -> some View {
+        modifier(TextEditorPlaceholderViewModifier(placeholder: placeholder, text: text, focused: focused))
     }
 }
