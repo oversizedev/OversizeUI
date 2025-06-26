d6c62f4e92b288393e75b5fae5bafe46c3587dae
Alexander Romanov <aromanov07@gmail.com>
2025-06-27 00:24:10 +0300

    Add isEmpty

---
+++ Sources/OversizeUI/Core/ViewModifier/IsEmpty.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2025 Alexander Romanov
+// NavigationPopGestureDisabler.swift, created on 07.06.2025
+//
+
+import SwiftUI
+
+public extension View {
+    var isEmpty: Bool {
+        self is EmptyView
+    }
+}
