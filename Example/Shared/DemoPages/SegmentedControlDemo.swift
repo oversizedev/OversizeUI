b778ab8084eac8eb279ac0f711808a9cd2b9bac2
Alexander Romanov <aromanov07@gmail.com>
2026-01-13 14:47:48 +0300

    Fix Demo

--- Example/Shared/DemoPages/SegmentedControlDemo.swift
+++ Example/Shared/DemoPages/SegmentedControlDemo.swift
@@ -62,7 +62,7 @@ struct SegmentedControlDemo: View {
                         Text(item)
                         Text("Subtitle")
                             .subheadline()
-                            .onSurfacePrimaryForeground()
+                            .onSurfacePrimary()
                     }
                 }
                 .segmentedControlStyle(SelectionOnlySegmentedControlStyle())
